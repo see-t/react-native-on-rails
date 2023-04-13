2f5897906a8a182447031f18a2b526bcd4980577
Bilal Korir <seekingk@live.com>
2023-04-13 21:30:15 +0800

    Configure eslint-plugin-simple-import-sort

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react'
 import type { PropsWithChildren } from 'react'
+import React from 'react'
 import {
   SafeAreaView,
   ScrollView,
@@ -16,7 +9,6 @@ import {
   useColorScheme,
   View,
 } from 'react-native'
-
 import {
   Colors,
   DebugInstructions,
