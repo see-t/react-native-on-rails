0f66e3090b1c2bcb959882af27340f9c899fc8a4
Bilal Korir <seekingk@live.com>
2023-04-14 09:11:38 +0800

    Link fs package

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import { Button, NativeBaseProvider } from 'native-base'
+import RNBootSplash from 'react-native-bootsplash'
 import { config, theme } from 'styles'
+
+RNBootSplash.hide({ duration: 500, fade: true })
 
 export default function App() {
   return (
