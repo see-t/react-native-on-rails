fe4532c66adb5e4caa3c24084a84ac4fac47e958
Bilal Korir <seekingk@live.com>
2023-04-16 18:22:09 +0800

    Update template 8

--- template.config.js
+++ template.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   placeholderName: 'ReactNativeOnRailsTemplate',
-  postInitScript: './script.js',
   templateDir: './template',
   titlePlaceholder: 'React Native On Rails Template',
 }
